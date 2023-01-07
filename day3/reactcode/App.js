d53c30dfab756a7413158bb7a166eba740a9d81b
Ankan Mal <ankanlogin@gmail.com>
2023-01-07 08:35:27 +0530

    FunctionalComponent using JSX

--- day3/reactcode/App.js
+++ day3/reactcode/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//creating three nested h1 inside div using jsx
-const titleJSX = (
-  <div className="title">
-    <h1 key="h1">Hi from header 1</h1>
-    <h2 key="h2">Hola from header 2</h2>
-    <h3 key="h3">Hey it is Header 3</h3>
-  </div>
-);
-console.log(titleJSX);
+//creating three nested h1 inside div using jsx but this is Functional Component
+const TitleFunction = () => {
+  return (
+    <div className="title">
+      <h1 key="h1">Hi from header 1</h1>
+      <h2 key="h2">Hola from header 2</h2>
+      <h3 key="h3">Hey it is Header 3</h3>
+    </div>
+  );
+};
+
+console.log(TitleFunction);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(titleJSX);
+root.render(<TitleFunction />);
