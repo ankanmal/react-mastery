c44a26ae91602862a11fcb5ee5ef33e7bee2f1b8
Ankan Mal <ankanlogin@gmail.com>
2023-01-07 08:41:29 +0530

    Passing attributes inside JSX

--- day3/reactcode/App.js
+++ day3/reactcode/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //creating three nested h1 inside div using jsx but this is Functional Component
+//Passing attribute inside jsx because with the use of {} in tag we can pass any valid javascript inside that.
+
+const name = "Ankan";
+
 const TitleFunction = () => {
   return (
     <div className="title">
-      <h1 key="h1">Hi from header 1</h1>
+      <h1 key="h1">Hi I am, {name} </h1>
       <h2 key="h2">Hola from header 2</h2>
       <h3 key="h3">Hey it is Header 3</h3>
     </div>
