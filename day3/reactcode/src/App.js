50ba8a72b698bcd401f96baad3e44cff5f93efcf
Ankan Mal <ankanlogin@gmail.com>
2023-01-24 10:49:00 +0530

    Uesd error and router error hooks

--- day3/reactcode/src/App.js
+++ day3/reactcode/src/App.js
@@ -6,6 +6,7 @@ import { Footer } from "./components/Footer";
 import Body from "./components/Body";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./components/About";
+import Error from "./components/Error";
 
 //creating a food delivery service name food kingdom
 
@@ -39,6 +40,7 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <Foodkingdom />,
+    errorElement: <Error />,
   },
   {
     path: "/about",
