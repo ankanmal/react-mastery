19fbedd50f13d747f4266f0ffc91af71c0e9a58b
Ankan Mal <ankanlogin@gmail.com>
2023-01-25 11:03:53 +0530

    Added Nested Comp Profile inside About

--- day3/reactcode/src/components/About.js
+++ day3/reactcode/src/components/About.js
@@ -1,8 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 const About = () => {
   return (
     <div>
       <h1>About Us</h1>
       <h2>Wlecome to the Food kingdom Website</h2>
+      <Outlet />
     </div>
   );
 };
