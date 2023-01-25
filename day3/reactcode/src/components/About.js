4d36645e5105ba84dff4f86fd28d14667f8ba720
Ankan Mal <ankanlogin@gmail.com>
2023-01-25 11:19:30 +0530

    created class component

--- day3/reactcode/src/components/About.js
+++ day3/reactcode/src/components/About.js
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
+import ProfileClass from "./ProfileClass";
+import ProfileFunc from "./Profile";
 
 const About = () => {
   return (
     <div>
       <h1>About Us</h1>
       <h2>Wlecome to the Food kingdom Website</h2>
-      <Outlet />
+      <ProfileFunc />
+      <ProfileClass />
     </div>
   );
 };
