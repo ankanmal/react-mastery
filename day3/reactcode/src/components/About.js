de179e0b7f99774d1d0858e310af484b0fa57d71
Ankan Mal <ankanlogin@gmail.com>
2023-01-25 12:16:06 +0530

    change about to class comp and logged lifecycle

--- day3/reactcode/src/components/About.js
+++ day3/reactcode/src/components/About.js
@@ -1,16 +1,27 @@
 import { Outlet } from "react-router-dom";
 import ProfileClass from "./ProfileClass";
 import ProfileFunc from "./Profile";
+import { Component } from "react";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <h2>Wlecome to the Food kingdom Website</h2>
-      <ProfileFunc name="Ankan Func" />
-      <ProfileClass name="Ankan class" />
-    </div>
-  );
-};
+class About extends Component {
+  constructor(props) {
+    console.log("Parent Constructor");
+    super(props);
+  }
+  componentDidMount() {
+    console.log("Parent Did Mount");
+  }
+
+  render() {
+    console.log("Parent Render");
+    return (
+      <div>
+        <h1>About Us</h1>
+        <h2>Wlecome to the Food kingdom Website</h2>
+        <ProfileClass name="Ankan class" />
+      </div>
+    );
+  }
+}
 
 export default About;
