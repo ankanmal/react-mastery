c7b55fd959108fdfa01748cd81b8e324e2755f83
Ankan Mal <ankanlogin@gmail.com>
2023-01-25 11:36:43 +0530

    Used State for class comp

--- day3/reactcode/src/components/ProfileClass.js
+++ day3/reactcode/src/components/ProfileClass.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 class Profile extends React.Component {
+  //to use state varible we call constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      count2: 0,
+    };
+  }
+
   render() {
     return (
       <div>
         <h1>Hello from Class Comp</h1>
         <h3>Name:{this.props.name}</h3>
+        <h3>Count:{this.state.count}</h3>
       </div>
     );
   }
