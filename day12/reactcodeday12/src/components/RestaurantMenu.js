503719f2734a94ad67926d7aa874e01ce8b7988e
Ankan Mal <ankanlogin@gmail.com>
2023-02-14 20:49:43 +0530

    Add to cart functionality works

--- day12/reactcodeday12/src/components/RestaurantMenu.js
+++ day12/reactcodeday12/src/components/RestaurantMenu.js
@@ -1,12 +1,19 @@
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { IMG_URL } from "../config";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import Shimmer from "./ShimmerUi";
+import { addToCart } from "../slice/cartSlice";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
+  const dispatch = useDispatch();
 
   const restaurant = useRestaurantMenu(resId);
+
+  const addItemToCart = () => {
+    dispatch(addToCart("grapes"));
+  };
 
   return restaurant === null ? (
     <Shimmer />
@@ -23,7 +30,16 @@ const RestaurantMenu = () => {
         <h2>Menu</h2>
         <ul>
           {Object.values(restaurant?.menu?.items).map((e) => (
-            <li key={e.id}>{e.name}</li>
+            <li key={e.id}>
+              {e.name}-
+              <button
+                className="bg-green-500 border
+             border-white rounded-xl"
+                onClick={() => addItemToCart()}
+              >
+                Add To Cart
+              </button>
+            </li>
           ))}
         </ul>
       </div>
