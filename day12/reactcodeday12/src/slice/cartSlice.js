de6feed721991366f4e751a94e9b989c0bffa557
Ankan Mal <ankanlogin@gmail.com>
2023-02-14 21:17:20 +0530

    Added remove from cart

--- day12/reactcodeday12/src/slice/cartSlice.js
+++ day12/reactcodeday12/src/slice/cartSlice.js
@@ -7,8 +7,21 @@ const cartSLice = createSlice({
     addToCart: (state, action) => {
       state.push(action.payload);
     },
+    removeFromCart: (state, action) => {
+      // let found = null;
+      // state.map((e) => {
+      //   if (e === action.payload) {
+      //     found = e;
+      //   }
+      //   return found;
+      // });
+      // if (!found) {
+      //   return state.filter((x) => x !== found);
+      // }
+      state.pop();
+    },
   },
 });
 
-export const { addToCart } = cartSLice.actions;
+export const { addToCart, removeFromCart } = cartSLice.actions;
 export default cartSLice.reducer;
